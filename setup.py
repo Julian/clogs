e97a87a44b1c969a0129bc1ac44776a14a62ad4b
Julian Berman <Julian+git@GrayVines.com>
2012-02-12 17:24:32 -0500

    Bump setup.py and ignore TODO

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from clogs import __version__
 
 # TODO: man page
+with open("README.rst") as f:
+    long_description = f.read()
 
-long_description = (
-"""clogs is a lightweight way to track code coverage over time using Ned
-Bachelder's fabulous coverage.py."""
-)
 
 setup(
     name="clogs",
-    version = __version__,
-    author = "Julian Berman",
-    author_email = "Julian+Clogs@GrayVines.com",
-    description = "clogs make coverage logs",
-    license = "MIT/X",
-    url = "http://github.com/Julian/clogs",
-    long_description = long_description,
-    packages = ["clogs", "clogs.tests"],
-    scripts = ["bin/clogs"],
-    install_requires = [
-        "coverage",
-        "GitPython",
-    ],
+    version=__version__,
+    author="Julian Berman",
+    author_email="Julian+Clogs@GrayVines.com",
+    description="clogs make coverage logs",
+    license="MIT/X",
+    url="http://github.com/Julian/clogs",
+    long_description=long_description,
+    packages=find_packages(),
+    scripts=["bin/clogs"],
+    include_package_data=True,
+    install_requires= ["coverage", "GitPython"],
 )
